c88361a790138d71cb4de206cc9686a4bef04175
Topman-14 <talktotimothy101@gmail.com>
2024-06-28 01:25:04 +0100

    fix bugs

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -53,7 +53,6 @@ const SettingsForm: React.FC<SettingsFormProps> = ({initialData}) => {
     const onSubmit = async (data: SettingsFormValues) => {
         try {
             setLoading(true)
-            console.log(data)
             await axios.patch(`/api/stores/${params.storeId}`, data)
             router.refresh();
             toast.success('Store updated!')
